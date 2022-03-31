8990ffe150b14e11ff49c3347e955ea1a8a91cf7
JIUMINGXIN <102711889+JIUMINGXIN@users.noreply.github.com>
2022-03-31 19:02:20 +0800

    1

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+	{
+	/*int a = 0;
+	scanf("%d", &a);
+	if (a % 2 == 0)
+		printf("Å¼Êý");
+	else if (a % 2 != 0)
+		printf("ÆæÊý");*/
+
+	//int i = 1;
+	//while (i <= 100)
+	//{
+	//	printf("%d", i);
+	//		i += 2;
+
+	//}
+
+
+
+	return 0;
+	}
