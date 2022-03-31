cd202005a83f9f2904bdcc7cc6a844f0cbd167df
JIUMINGXIN <102711889+JIUMINGXIN@users.noreply.github.com>
2022-03-31 19:28:36 +0800

    1.1

--- 2.cpp
+++ 2.cpp
@@ -5,6 +5,7 @@
 
 int main()
 	{
+	
 	/*int a = 0;
 	scanf("%d", &a);
 	if (a % 2 == 0)
@@ -19,8 +20,40 @@ int main()
 	//		i += 2;
 
 	//}
-
-
+	
+		int n = 5;/*3.2*/
+		int m = 3;
+	switch (n)
+	{
+		
+	case 1:
+		m++;
+	case 2:
+		n++;
+	case 3:
+		switch (m)
+		{
+		case 1:
+			n++;
+		case 2:
+			m++;
+			n++;
+			break;
+		default:
+			n--;
+			m--;
+		}
+		
+	case 4:
+		m++;
+		break;
+		
+	default:
+		n++;
+		m--;
+		break;
+	}
+printf("n=%d m=%d\n", n, m);
 
 	return 0;
 	}
