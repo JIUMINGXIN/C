df63cc527b79aa6cc668b4d84a4437acf9a8426c
JIUMINGXIN <102711889+JIUMINGXIN@users.noreply.github.com>
2022-04-06 12:54:32 +0800

    Add files via upload

--- 2.cpp
+++ 2.cpp
@@ -5,62 +5,82 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-void game()
-{
-	int q = 0;
-	int w = 0;
-	q = rand()%100+1;
-	while(1)
-	{
-		printf("请猜数字:\n");
-		scanf("%d", &w);
-		if (w > q)
-		{
-			printf("猜大了\n");
-
-		}
-		else if (w < q)
-		{
-			printf("猜小了\n");
-		}
-		else
-		{
-			printf("猜到了\n");
-			break;
-		}
-	}
-
-}
-void menu()
-{
-	printf("*******************\n");
-	printf("*** 1.play 0.no ***\n");
-	printf("*******************\n");
-}
-int main()
-{
-	srand(unsigned int(time(NULL)));
-	int a = 0;
-	do
-	{
-		menu();
-		printf("请输入数字:");
-		scanf("%d", &a);
-		switch (a)
-		{
-		case 1:
-			game();
-			printf("猜数字:\n");
-			break;
-		case 0:
-			printf("游戏结束!");
-			break;
-		default:
-			printf("重新输入:\n");
-			break;
-		}
-			
-	} while (a!=0);
-
-	return 0;
-}
+//void game()
+//{
+//	int q = 0;
+//	int w = 0;
+//	q = rand()%100+1;
+//	while(1)
+//	{
+//		printf("请猜数字:\n");
+//		scanf("%d", &w);
+//		if (w > q)
+//		{
+//			printf("猜大了\n");
+//
+//		}
+//		else if (w < q)
+//		{
+//			printf("猜小了\n");
+//		}
+//		else
+//		{
+//			printf("猜到了\n");
+//			break;
+//		}
+//	}
+//
+//}
+//void menu()
+//{
+//	printf("*******************\n");
+//	printf("*** 1.play 0.no ***\n");
+//	printf("*******************\n");
+//}
+//int main()
+//{
+//	srand(unsigned int(time(NULL)));
+//	int a = 0;
+//	do
+//	{
+//		menu();
+//		printf("请输入数字:");
+//		scanf("%d", &a);
+//		switch (a)
+//		{
+//		case 1:
+//			game();
+////			printf("猜数字:\n");
+////			break;
+////		case 0:
+////			printf("游戏结束!");
+////			break;
+////		default:
+////			printf("重新输入:\n");
+////			break;
+////		}
+////			
+////	} while (a!=0);
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	char a[20] = { 0 };
+//system("shutdown -s -t 30");
+//	while (1)
+//	{
+//		
+//
+//		printf("请注意你的电脑即将在30秒内关机，输入我是鸡将停止关机\n");
+//
+//		scanf("%s", a);
+//		if (strcmp(a, "我是鸡") == 0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//	}
+//	return 0;		
+//}
